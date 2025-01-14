8e5e87c39775c9870c9854f375f107583dbf8afa
shubham-applore <shubham.gupta@applore.in>
2025-01-14 13:13:24 +0530

    initial release

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="file://10.30.28.203/SharedFolder/" target="_blank" rel="noopener noreferrer">
+          Link
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
